eb92a255dd2e674dec6203000e469f4b1574e36e
Andrey Sitnik <andrey@sitnik.ru>
2017-06-25 05:01:28 -0300

    Look into package.json for files

--- cli.js
+++ cli.js
@@ -3,18 +3,71 @@
 
 const yargs = require('yargs')
 const path = require('path')
+const fs = require('fs')
 
 const getSize = require('.')
 
 const argv = yargs
+  .usage('$0 [FILES]')
   .example('$0 ./index.js ./extra.js')
+  .epilog('If you miss files, size-limit will take main file ' +
+          'from package.json')
   .locale('en')
   .version()
   .help()
   .argv
 
-const files = argv['_'].map(i => path.join(process.cwd(), i))
+function isRoot (dir) {
+  if (process.platform === 'win32') {
+    return /^\w:[\\/]*$/.test(dir)
+  } else {
+    return dir === '/'
+  }
+}
 
-getSize.apply({ }, files).then(size => {
+function findPackage (dir) {
+  if (isRoot(dir)) return Promise.resolve(false)
+  const file = path.join(dir, 'package.json')
+
+  return new Promise(resolve => {
+    fs.readFile(file, (err, data) => {
+      if (err) {
+        resolve(findPackage(path.dirname(dir)))
+      } else {
+        resolve({ package: JSON.parse(data), dir })
+      }
+    })
+  })
+}
+
+let getFiles
+if (argv['_'].length > 0) {
+  getFiles = Promise.resolve(argv['_'].map(i => {
+    if (path.isAbsolute(i)) {
+      return i
+    } else {
+      return path.join(process.cwd(), i)
+    }
+  }))
+} else {
+  getFiles = findPackage(process.cwd()).then(result => {
+    if (result) {
+      return [path.join(result.dir, result.package.main || 'index.js')]
+    } else {
+      return []
+    }
+  })
+}
+
+getFiles.then(files => {
+  if (files.length === 0) {
+    throw new Error('Specify project files ' +
+                    'or run in project dir with package.json')
+  }
+  return getSize.apply({ }, files)
+}).then(size => {
   process.stdout.write(`${ size }\n`)
+}).catch(e => {
+  process.stderr.write(`${ e.stack }\n`)
+  process.exit(1)
 })
--- test/cli.test.js
+++ test/cli.test.js
@@ -1,9 +1,14 @@
 'use strict'
 
 const spawn = require('cross-spawn')
+const path = require('path')
 
-function run () {
-  const cli = spawn('./cli.js', Array.prototype.slice.call(arguments, 0))
+function fixture (file) {
+  return path.join(__dirname, 'fixtures', file)
+}
+
+function run (args, options) {
+  const cli = spawn(path.join(__dirname, '../cli.js'), args, options)
   return new Promise(resolve => {
     let out = ''
     cli.stdout.on('data', data => {
@@ -12,26 +17,57 @@ function run () {
     cli.stderr.on('data', data => {
       out += data.toString()
     })
-    cli.on('close', () => {
-      resolve(out)
+    cli.on('close', code => {
+      resolve({ code, out })
     })
   })
 }
 
 it('returns help', () => {
-  return run('--help').then(out => {
-    expect(out).toContain('Examples:')
+  return run(['--help']).then(result => {
+    expect(result.code).toEqual(0)
+    expect(result.out).toContain('Examples:')
   })
 })
 
 it('returns version', () => {
-  return run('--version').then(out => {
-    expect(out).toMatch(/\d+\.\d+\.\d+/)
+  return run(['--version']).then(result => {
+    expect(result.code).toEqual(0)
+    expect(result.out).toMatch(/\d+\.\d+\.\d+/)
   })
 })
 
 it('returns size', () => {
-  return run('test/fixtures/empty.js').then(out => {
-    expect(out).toEqual('0\n')
+  return run(['test/fixtures/empty.js']).then(result => {
+    expect(result.code).toEqual(0)
+    expect(result.out).toEqual('0\n')
+  })
+})
+
+it('supports absolute path', () => {
+  return run([path.join(__dirname, 'fixtures/empty.js')]).then(result => {
+    expect(result.code).toEqual(0)
+    expect(result.out).toEqual('0\n')
+  })
+})
+
+it('reads package.json', () => {
+  return run([], { cwd: fixture('main/dir') }).then(result => {
+    expect(result.code).toEqual(0)
+    expect(result.out).toEqual('22\n')
+  })
+})
+
+it('uses index.js by default', () => {
+  return run([], { cwd: fixture('index/dir') }).then(result => {
+    expect(result.code).toEqual(0)
+    expect(result.out).toEqual('20\n')
+  })
+})
+
+it('shows errors', () => {
+  return run(['unknown.js']).then(result => {
+    expect(result.code).toEqual(1)
+    expect(result.out).toContain('unknown.js')
   })
 })
