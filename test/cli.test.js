dbe6733f45771fe81a269f89ba6ae98b5bc69121
Andrey Sitnik <andrey@sitnik.ru>
2017-07-02 17:21:01 -0300

    Move all exit code checks to bottom

--- test/cli.test.js
+++ test/cli.test.js
@@ -25,139 +25,139 @@ function run (args, options) {
 
 it('returns help', () => {
   return run(['--help']).then(result => {
-    expect(result.code).toEqual(0)
     expect(result.out).toContain('Examples:')
+    expect(result.code).toEqual(0)
   })
 })
 
 it('returns version', () => {
   return run(['--version']).then(result => {
-    expect(result.code).toEqual(0)
     expect(result.out).toMatch(/\d+\.\d+\.\d+/)
+    expect(result.code).toEqual(0)
   })
 })
 
 it('returns size', () => {
   return run(['test/fixtures/empty.js']).then(result => {
-    expect(result.code).toEqual(0)
     expect(result.out).toEqual('\n' +
-      '  Package size: 0 B\n' +
-      '  With all dependencies, minified and gzipped\n' +
-      '\n')
+    '  Package size: 0 B\n' +
+    '  With all dependencies, minified and gzipped\n' +
+    '\n')
+    expect(result.code).toEqual(0)
   })
 })
 
 it('uses different units', () => {
   return run(['test/fixtures/big.js']).then(result => {
-    expect(result.code).toEqual(0)
     expect(result.out).toContain('2.38 KB\n')
+    expect(result.code).toEqual(0)
   })
 })
 
 it('supports absolute path', () => {
   return run([path.join(__dirname, 'fixtures/empty.js')]).then(result => {
-    expect(result.code).toEqual(0)
     expect(result.out).toContain('0 B\n')
+    expect(result.code).toEqual(0)
   })
 })
 
 it('reads package.json', () => {
   return run([], { cwd: fixture('main/dir') }).then(result => {
-    expect(result.code).toEqual(0)
     expect(result.out).toContain('22 B\n')
+    expect(result.code).toEqual(0)
   })
 })
 
 it('uses index.js by default', () => {
   return run([], { cwd: fixture('index/dir') }).then(result => {
-    expect(result.code).toEqual(0)
     expect(result.out).toContain('20 B\n')
+    expect(result.code).toEqual(0)
   })
 })
 
 it('shows resolve errors', () => {
   return run(['unknown.js']).then(result => {
-    expect(result.code).toEqual(1)
     expect(result.out).toContain('unknown.js')
     expect(result.out).toMatch(/^Can't resolve/)
+    expect(result.code).toEqual(1)
   })
 })
 
 it('shows package.json errors', () => {
   const cwd = path.dirname(path.dirname(__dirname))
   return run([], { cwd }).then(result => {
-    expect(result.code).toEqual(1)
     expect(result.out).toEqual(
       'Specify project files or run in project dir with package.json\n')
+    expect(result.code).toEqual(1)
   })
 })
 
 it('shows limit', () => {
   return run(['2KB', 'test/fixtures/empty.js']).then(result => {
-    expect(result.code).toEqual(0)
     expect(result.out).toContain('Size limit:   2 KB\n')
+    expect(result.code).toEqual(0)
   })
 })
 
 it('shows small K', () => {
   return run(['2kB', 'test/fixtures/empty.js']).then(result => {
-    expect(result.code).toEqual(0)
     expect(result.out).toContain('Size limit:   2 KB\n')
+    expect(result.code).toEqual(0)
   })
 })
 
 it('allows space in limit', () => {
   return run(['2', 'KB', 'test/fixtures/empty.js']).then(result => {
-    expect(result.code).toEqual(0)
     expect(result.out).toContain('Size limit:   2 KB\n')
+    expect(result.code).toEqual(0)
   })
 })
 
 it('allows fractional in limit', () => {
   return run(['2.20KB', 'test/fixtures/empty.js']).then(result => {
-    expect(result.code).toEqual(0)
     expect(result.out).toContain('Size limit:   2.2 KB\n')
+    expect(result.code).toEqual(0)
   })
 })
 
 it('allows unitless limit', () => {
   return run(['2048', 'test/fixtures/empty.js']).then(result => {
-    expect(result.code).toEqual(0)
     expect(result.out).toContain('Size limit:   2 KB\n')
+    expect(result.code).toEqual(0)
   })
 })
 
 it('allows bytes', () => {
   return run(['2048B', 'test/fixtures/empty.js']).then(result => {
-    expect(result.code).toEqual(0)
     expect(result.out).toContain('Size limit:   2 KB\n')
+    expect(result.code).toEqual(0)
   })
 })
 
 it('allows space and bytes', () => {
   return run(['2048', 'B', 'test/fixtures/empty.js']).then(result => {
-    expect(result.code).toEqual(0)
     expect(result.out).toContain('Size limit:   2 KB\n')
+    expect(result.code).toEqual(0)
   })
 })
 
 it('checks limits', () => {
   return run(['2KB', 'test/fixtures/big.js']).then(result => {
-    expect(result.code).toEqual(3)
     expect(result.out).toContain('exceeded the size limit')
+    expect(result.code).toEqual(3)
   })
 })
 
 it('shows analyzer', () => {
   return run(['--why', 'test/fixtures/empty.js']).then(result => {
-    expect(result.code).toEqual(0)
     expect(result.out).toContain('Webpack Bundle Analyzer')
+    expect(result.code).toEqual(0)
   })
 })
 
 it('uses Babili', () => {
   return run(['--babili', 'test/fixtures/es2016.js']).then(result => {
-    expect(result.code).toEqual(0)
     expect(result.out).toContain('39 B\n')
+    expect(result.code).toEqual(0)
   })
 })
