8ba864fd19ec6af7363ee005f50bf5598c8d19bc
Sebastian Werner <s.werner@sebastian-software.de>
2017-08-15 14:21:47 +0200

    Fixed CSS modules test

--- test/index.test.js
+++ test/index.test.js
@@ -46,7 +46,13 @@ it('support images', () => {
 
 it('support css', () => {
   return getSize(fixture('css/index')).then(size => {
-    expect(size).toEqual(53)
+    expect(size).toEqual(2172)
+  })
+})
+
+it('support css modules', () => {
+  return getSize(fixture('cssmodules/index')).then(size => {
+    expect(size).toEqual(2205)
   })
 })
 
