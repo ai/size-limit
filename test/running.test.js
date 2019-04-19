485549f19cf0bb3e16f184d5ce3fe905a70c1e88
Andrey Sitnik <andrey@sitnik.ru>
2019-04-19 19:50:13 -0400

    Increase running test timeout

--- test/running.test.js
+++ test/running.test.js
@@ -6,6 +6,8 @@ function fixture (name) {
   return join(__dirname, 'fixtures', `${ name }.js`)
 }
 
+jest.setTimeout(10000)
+
 it('calculates running time', async () => {
   let size = await getSize(fixture('bad/index'))
   expect(size.running > 0.15).toBeTruthy()
