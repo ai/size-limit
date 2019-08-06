701235f474e15db1492fe4b64196eca564bb8c97
Andrey Sitnik <andrey@sitnik.ru>
2019-08-06 03:15:24 -0400

    Fix CI

--- packages/size-limit/test/bin.test.js
+++ packages/size-limit/test/bin.test.js
@@ -20,13 +20,17 @@ function run (args, env = { }) {
 }
 
 it('runs only on first job in Travis CI', async () => {
-  let { out, code } = await run([], { TRAVIS: '1', TRAVIS_JOB_NUMBER: '1.2' })
+  let { out, code } = await run([], {
+    TRAVIS: '1', TRAVIS_JOB_NUMBER: '1.2'
+  })
   expect(out).toContain('first CI job')
   expect(code).toEqual(0)
 })
 
 it('passes process to runner', async () => {
-  let { out, code } = await run(['--version'])
+  let { out, code } = await run(['--version'], {
+    TRAVIS: '1', TRAVIS_JOB_NUMBER: '1.1'
+  })
   expect(out).toMatch(/size-limit \d+.\d+.\d+/)
   expect(code).toEqual(0)
 })
