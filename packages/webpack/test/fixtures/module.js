11bd89f511825fc3c85531d424841dc4c5701c93
Brian Schlenker <bschlenk@umich.edu>
2020-02-26 09:18:47 -0500

    Add import option to check config
    This option allows specifying a specific exported object to be imported
    from the configured path, allowing testing for tree shaking.

---
+++ packages/webpack/test/fixtures/module.js
@@ -0,0 +1,11 @@
+export function methodA () {
+  console.log('AAAAAAAAAAA')
+  console.log('AAAAAAAAAAA')
+  console.log('AAAAAAAAAAA')
+  console.log('AAAAAAAAAAA')
+  console.log('AAAAAAAAAAA')
+}
+
+export function methodB () {
+  console.log('BBBBBBBBBBB')
+}
