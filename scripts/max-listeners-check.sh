712b9bef1d425bee8f067bce9e975f353c484367
Pavel Pustovalov <pustovalov.pavel@gmail.com>
2020-08-11 01:17:36 +0300

    Fix typo (#195)

--- scripts/max-listeners-check.sh
+++ scripts/max-listeners-check.sh
@@ -2,7 +2,7 @@
 
 file_path="node-warnings.logs"
 cat /dev/null > $file_path
-node --redirect-warnings=$file_path packages/size-limit/test/max-litsteners-test.js
+node --redirect-warnings=$file_path packages/size-limit/test/max-listeners-test.js
 
 if grep -q MaxListenersExceededWarning "$file_path"; then
   echo "Possible EventEmitter memory leak detected"
