a6ced16389788fcf219b051f9944e51731f47748
Sergey Melyukov <s.melukov@gmail.com>
2021-10-08 20:06:23 +0300

    Update why screenshot
    Update help text

--- packages/size-limit/create-help.js
+++ packages/size-limit/create-help.js
@@ -43,7 +43,9 @@ module.exports = process => {
         '',
         b('Webpack options:'),
         `  ${y('--why')}              Show package content`,
-        `  ${y('--other-stats')}      Path to another stats.json for showing in the UI`,
+        `  ${y(
+          '--other-stats'
+        )}      Path to stats.json from another build to compare`,
         `  ${y('--save-bundle DIR')}  Put build files to check them by hand`,
         `  ${y('--clean-dir')}        Remove build files folder before start`
       )
