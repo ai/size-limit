2389e0be40400decf5e1e47a197fe9c0e753de27
Andrey Sitnik <andrey@sitnik.ru>
2021-10-10 13:07:38 +0200

    Improve tree shaking test

--- packages/size-limit/test/fixtures/integration-esm/index.js
+++ packages/size-limit/test/fixtures/integration-esm/index.js
@@ -1,3 +1,3 @@
-let { VERY_LONG_NAME_FOR_CONST_TO_TEST_TREE_SHAKING } = require('./lib')
+let { VERY_LONG_NAME_FOR_CONST_TO_TEST_TREE_SHAKING, method } = require('./lib')
 
-module.exports = { VERY_LONG_NAME_FOR_CONST_TO_TEST_TREE_SHAKING }
+module.exports = { VERY_LONG_NAME_FOR_CONST_TO_TEST_TREE_SHAKING, method }
--- packages/size-limit/test/fixtures/integration-esm/lib/index.js
+++ packages/size-limit/test/fixtures/integration-esm/lib/index.js
@@ -1,3 +1,7 @@
 const VERY_LONG_NAME_FOR_CONST_TO_TEST_TREE_SHAKING = 1
 
-module.exports = { VERY_LONG_NAME_FOR_CONST_TO_TEST_TREE_SHAKING }
+function method() {
+  console.log('1111111111111111')
+}
+
+module.exports = { VERY_LONG_NAME_FOR_CONST_TO_TEST_TREE_SHAKING, method }
