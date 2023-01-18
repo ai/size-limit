4f5c8b82b4eddb18c759abb4a8c99020ee793279
Andrey Sitnik <andrey@sitnik.ru>
2023-01-18 12:35:28 +0100

    Remove CI job number

--- packages/size-limit/bin.js
+++ packages/size-limit/bin.js
@@ -1,13 +1,3 @@
 #!/usr/bin/env node
 
-let ciJobNumber = require('ci-job-number')
-let { yellow } = require('picocolors')
-
-if (ciJobNumber() !== 1) {
-  process.stderr.write(
-    yellow('Size Limit runs only on first CI job, to save CI resources\n')
-  )
-  process.exit(0)
-}
-
 require('./run.js')(process)
