82de5af0bfc85aaa5e36c8fe58a5a4a490e4f34f
Andrey Sitnik <andrey@sitnik.ru>
2023-02-12 21:27:45 +0100

    Clean up code

--- packages/esbuild-why/valid-filename.js
+++ packages/esbuild-why/valid-filename.js
@@ -1,28 +1,11 @@
-// from https://github.com/sindresorhus/valid-filename/blob/main/index.js
-function isValidFilename(string) {
-  if (!string || string.length > 255) {
-    return false;
+module.exports = {
+  isValidFilename(string) {
+    if (!string || string.length > 255 || string === '.' || string === '..') {
+      return false
+    }
+    return (
+      !/[<>:"/\\|?*\u0000-\u001F]/g.test(string) &&
+      !/^(con|prn|aux|nul|com\d|lpt\d)$/i.test(string)
+    )
   }
-
-  if (filenameReservedRegex().test(string) || windowsReservedNameRegex().test(string)) {
-    return false;
-  }
-
-  if (string === '.' || string === '..') {
-    return false;
-  }
-
-  return true;
 }
-
-// from https://github.com/sindresorhus/filename-reserved-regex/blob/main/index.js
-function filenameReservedRegex() {
-  // eslint-disable-next-line no-control-regex
-  return /[<>:"/\\|?*\u0000-\u001F]/g;
-}
-
-function windowsReservedNameRegex() {
-  return /^(con|prn|aux|nul|com\d|lpt\d)$/i;
-}
-
-module.exports = { isValidFilename }
