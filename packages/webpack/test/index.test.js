6243789e9fc9bab4f8aac1854f95c6ea6880d234
Andrey Sitnik <andrey@sitnik.ru>
2023-02-12 18:14:26 +0100

    Update dependencies

--- packages/webpack/test/index.test.js
+++ packages/webpack/test/index.test.js
@@ -51,7 +51,7 @@ it('uses webpack to make bundle', async () => {
         webpackOutput: config.checks[0].webpackOutput,
         webpackConfig: config.checks[0].webpackConfig,
         bundles: [join(config.checks[0].webpackOutput, 'index.js')],
-        size: 2472
+        size: 2458
       }
     ]
   })
