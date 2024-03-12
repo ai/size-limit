986857c0d8f61f2b7d0cbabce8c6d746f16fd992
Andrey Sitnik <andrey@sitnik.ru>
2024-03-12 15:41:17 +0100

    Revert "Fix StatoscopeWebpackPlugin import to support default export (#357)"
    This reverts commit cb1844f5d602c3576149bb68f01d0054f3452cf6.

--- packages/webpack-why/index.js
+++ packages/webpack-why/index.js
@@ -6,7 +6,7 @@ function addStatoscope(limitConfig, check, webpackConfig) {
     let shouldOpen = process.env.NODE_ENV !== 'test' && !limitConfig.saveBundle
 
     webpackConfig.plugins.push(
-      new StatoscopeWebpackPlugin.default({
+      new StatoscopeWebpackPlugin({
         additionalStats: [limitConfig.compareWith, check.compareWith].filter(
           Boolean
         ),
@@ -24,7 +24,7 @@ function addStatoscope(limitConfig, check, webpackConfig) {
     )
   } else if (limitConfig.saveBundle) {
     webpackConfig.plugins.push(
-      new StatoscopeWebpackPlugin.default({
+      new StatoscopeWebpackPlugin({
         open: false,
         saveReportTo: join(limitConfig.saveBundle, 'report.html'),
         saveStatsTo: join(limitConfig.saveBundle, 'stats.json'),
