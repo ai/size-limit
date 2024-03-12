ee94b70228668898f3d02f857156a4adaa438660
Arya Emami <aryaemami59@yahoo.com>
2024-03-12 04:32:20 -0500

    Fix minor type issue in JSDocs

--- packages/size-limit/index.js
+++ packages/size-limit/index.js
@@ -8,7 +8,7 @@ export { SizeLimitError } from './size-limit-error.js'
 /**
  * Run Size Limit and return the result
  *
- * @param  {functions[]} plugins   The list of plugins like `@size-limit/time`
+ * @param  {Function[]} plugins   The list of plugins like `@size-limit/time`
  * @param  {string[]|object} files Path to files or internal config
  * @return {Promise<object>}     Project size
  */
