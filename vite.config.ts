3556e71d2e80c2100c51639a8517be768b2eca2d
Arya Emami <aryaemami59@yahoo.com>
2024-03-12 02:07:05 -0500

    Increase the `timeout` to accommodate the new added number of tests

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
         'packages/preset-app/index.js'
       ]
     },
-    testTimeout: 10000,
+    testTimeout: 20_000,
     watchExclude: ['**/fixtures', '**/dist', '**/out']
   }
 })
