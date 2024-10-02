909a1bb2740522f09f8421f398ff4688f8b247cb
Andrey Sitnik <andrey@sitnik.ru>
2024-10-02 21:53:03 +0000

    Remove code for old Node.js

--- packages/size-limit/rm.js
+++ packages/size-limit/rm.js
@@ -1,13 +1,5 @@
-import { existsSync } from 'node:fs'
 import fs from 'node:fs/promises'
 
 export async function rm(dir) {
-  if (!fs.rm) {
-    /* c8 ignore next 3 */
-    if (existsSync(dir)) {
-      await fs.rmdir(dir, { recursive: true })
-    }
-  } else {
-    await fs.rm(dir, { force: true, recursive: true })
-  }
+  await fs.rm(dir, { force: true, recursive: true })
 }
