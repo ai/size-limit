aab8d11912bb680a86cacdb616388fb809a74fe8
Deepanshu Pal <40927968+DeepanshuPal@users.noreply.github.com>
2026-09-26 19:28:28 +0530

    Document and test opt-in missing checks

--- packages/size-limit/create-help.js
+++ packages/size-limit/create-help.js
@@ -40,6 +40,7 @@ export default process => {
       `  ${y('--limit LIMIT')}     Set size or running time limit for files`,
       `  ${y('--json')}            Show results in JSON format`,
       `  ${y('--hide-passed')}     Print only failed files`,
+      `  ${y('--ignore-missing')}  Skip checks whose paths match no files`,
       `  ${y('--highlight-less')}  Highlight files with less than limit size`,
       `  ${y('--help')}            Display this help`,
       `  ${y('--watch')}           Runs in watch mode`,
